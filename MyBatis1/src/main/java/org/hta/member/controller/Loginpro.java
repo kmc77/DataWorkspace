265cb205777351127b4b6948cd4f3b4009bb2d7e
kmc77 <k_minchoi@naver.com>
2023-08-21 17:53:00 +0900

    Logout.java

--- MyBatis1/src/main/java/org/hta/member/controller/Loginpro.java
+++ MyBatis1/src/main/java/org/hta/member/controller/Loginpro.java
@@ -1,19 +1,56 @@
 package org.hta.member.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.hta.member.dao.MemberDao;
+import org.hta.member.domain.Member;
 
 public class Loginpro implements Action {
     
     @Override
     public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        ActionForward forward = new ActionForward();
-        forward.setRedirect(false);
-        forward.setPath("jsp/main.jsp");
-        return forward;
-}
+    	String id = request.getParameter("id");
+    	String password = request.getParameter("password");
+    	
+    	Member mem = new Member();
+    	mem.setId(id);
+    	mem.setPassword(password);
+    	
+    	MemberDao md = new MemberDao();
+    	int result = md.chk(mem);
+    	
+    	response.setContentType("test/html;charset=utf-8");
+    	PrintWriter out = response.getWriter();
+    	if(result == 1) { //아이디와 비번이 같은 경우
+    		HttpSession session = request.getSession();
+    		session.setAttribute("id", mem.getId());
+    		out.println("<script>");
+        	out.println("alert('환영합니다~." + id + "님~');");
+        	out.println("location.href='main.net'");
+        	out.println("</script>");
+        	out.close();
+
+    	} else if (result == -1) { //아이디는 같고 비번이 다른 경우
+    		out.println("<script>");
+        	out.println("alert('비밀번호가 다릅니다.');");
+        	out.println("history.go(-1);");
+        	out.println("</script>");
+        	out.close();
+    	} else if (result == 0) { 
+    		out.println("<script>");
+        	out.println("alert('ID가 존재하지 않습니다.');");
+        	out.println("history.go(-1);");
+        	out.println("</script>");
+        	out.close();	
+		}
+		return null;
+	}
+
 }
--- MyBatis1/src/main/java/org/hta/member/controller/Logout.java
+++ MyBatis1/src/main/java/org/hta/member/controller/Logout.java
@@ -1,19 +1,32 @@
 package org.hta.member.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Logout implements Action {
     
     @Override
     public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        ActionForward forward = new ActionForward();
-        forward.setRedirect(false);
-        forward.setPath("jsp/main.jsp");
-        return forward;
+    	
+	HttpSession session = request.getSession();
+    	
+    	response.setContentType("text/html;charset=utf-8");
+    	PrintWriter out = response.getWriter();
+    	
+    	session.invalidate();
+    	
+    	out.println("<script>");
+    	out.println("alert('로그아웃 되었습니다.');");
+    	out.println("location.href='loginForm.net'");
+    	out.println("</script>");
+    	
+       
+        return null;
     }
 }
