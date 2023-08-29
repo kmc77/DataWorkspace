08cc9a716a70d98f5d4e0900dd981e4f542342c4
kmc77 <k_minchoi@naver.com>
2023-08-29 11:13:50 +0900

    BoardController

--- sts/Spring4_MVC_BOARD/src/main/java/com/naver/myhome4/service/CommentServicelmpl.java
+++ sts/Spring4_MVC_BOARD/src/main/java/com/naver/myhome4/service/CommentServicelmpl.java
@@ -3,10 +3,12 @@ package com.naver.myhome4.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.naver.myhome4.dao.CommentDAO;
 import com.naver.myhome4.domain.Comment;
 
+@Service
 public class CommentServicelmpl implements CommentService{
 	
 	private CommentDAO dao;
