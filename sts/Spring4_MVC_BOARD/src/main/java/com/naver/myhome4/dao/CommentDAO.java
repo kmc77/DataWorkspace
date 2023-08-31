535dc5a247c52c87e9edb7071ca5bfeceae0e6ce
kmc77 <k_minchoi@naver.com>
2023-08-31 15:27:16 +0900

    getCommentList

--- sts/Spring4_MVC_BOARD/src/main/java/com/naver/myhome4/dao/CommentDAO.java
+++ sts/Spring4_MVC_BOARD/src/main/java/com/naver/myhome4/dao/CommentDAO.java
@@ -1,8 +1,13 @@
 package com.naver.myhome4.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.naver.myhome4.domain.Comment;
 
 @Repository
 public class CommentDAO {
@@ -16,5 +21,10 @@ public class CommentDAO {
 	
 	public int getListCount(int board_num) {
 		return sqlSession.selectOne("Comments.count",board_num);
+	}
+	
+	public List<Comment> getCommentList(Map<String, Integer> map) {
+		return sqlSession.selectList("Comments.list", map);
+		
 	}
 }
