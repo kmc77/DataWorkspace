010f5eb253a1c0524af46165d6c8dd3e0ab139d2
kmc77 <k_minchoi@naver.com>
2023-09-06 11:38:12 +0900

    security-context4.xml

---
+++ sts/Spring10_Security/src/main/java/com/naver/security/LoginSuccessHandler.java
@@ -0,0 +1,49 @@
+package com.naver.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
+
+import com.naver.myhome4.domain.Member;
+
+//AuthenticationFailureHandler : 로그인 성공 후 처리할 작업을 해야할 때 사용하는 인터페이스입니다.
+@Service
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginSuccessHandler.class);
+	
+
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		logger.info("로그인 성공 : LoginSuccessHandler ");
+		String url = request.getContextPath() + "/board/list";
+		response.sendRedirect(url);
+		
+	}
+	
+	
+}
+
+	
