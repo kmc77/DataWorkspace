a1f7bf8737d747fa6c5f151e0343709c4fd6c1e9
kmc77 <k_minchoi@naver.com>
2023-09-07 09:35:53 +0900

    MemberMapper

--- sts/Spring4_MVC_BOARD2/src/main/java/com/naver/myhome3/config/RootConfig.java
+++ sts/Spring4_MVC_BOARD2/src/main/java/com/naver/myhome3/config/RootConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
  root-context.xml 역할
  
  @Configuration
- 이 클래스가 설정 클래스임을 알려주는 annotation입니다.
+ 1. 이 클래스가 설정 클래스임을 알려주는 메서드입니다.
+ 2. 클래스가 하나 이상의 
  */
 @Configuration
 public class RootConfig {
