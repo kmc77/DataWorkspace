b2d19d3896e43e9d762423724b4d2eae568c6432
kmc77 <k_minchoi@naver.com>
2023-09-12 09:15:59 +0900

    Spring10_MVC_Security_War2 (최종)

---
+++ Spring Boot2/Spring10_MVC_Security_War2/src/main/java/com/naver/myhome/Spring10MvcSecurityWar1Application.java
@@ -0,0 +1,13 @@
+package com.naver.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10MvcSecurityWar1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring10MvcSecurityWar1Application.class, args);
+	}
+
+}
